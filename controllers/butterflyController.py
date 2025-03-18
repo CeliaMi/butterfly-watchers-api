5c9e84d37e357c42d027c4600208716f8c9cd598
Celia <celiamillanalemany@gmail.com>
2025-03-18 16:37:01 +0100

    added GET, GET_ID, POST methods

--- controllers/butterflyController.py
+++ controllers/butterflyController.py
@@ -1,11 +1,36 @@
 from sqlalchemy.orm import Session
-from models import butterflyModel 
+from models.butterflyModel import Butterfly
 from database import database_connection
 from fastapi import Depends
+from schemas.buterflySchema import ButterflyCreate
 
+# Todas estas funciones realizan consultas (query) en la base de datos
+# Voy a especificar en un comentario lo que consulta cada una de ellas
 
-
-# Función para obtener todas las mariposas
+# Función que consulta READ todas las mariposas
 def get_butterflies(db: Session = Depends(database_connection.get_db)): 
-    return db.query(butterflyModel.Butterfly).all()
+    return db.query(Butterfly).all()
+
+# Función que consulta READ una mariposa por su ID método filter
+def get_butterfly_by_id(db: Session, butterfly_id: int):
+    butterfly = db.query(Butterfly).filter(Butterfly.id == butterfly_id).first()
+    return butterfly
+
+# # Función que CREA una mariposa método add
+# # El método add() agrega un objeto a la sesión actual
+# # El método commit() guarda los cambios en la base de datos
+# # El método refresh() actualiza el objeto con los cambios realizados en la base de datos
+def create_butterfly(db: Session, butterfly: ButterflyCreate):
+    new_butterfly = Butterfly(**butterfly.dict())
+    db.add(new_butterfly)
+    db.commit()
+    db.refresh(new_butterfly)
+    return new_butterfly
+
+
+# # Función que ACTUALIZA una mariposa método update
+# def update_butterfly(db: Session, butterfly_id: int, butterfly: Butterfly):
+#     db.query(Butterfly).filter(Butterfly.id == butterfly_id).update(butterfly.dict())
+#     db.commit()
+#     return butterfly
 
--- routes/butterflyRouter.py
+++ routes/butterflyRouter.py
@@ -1,10 +1,35 @@
 from fastapi import APIRouter
 from controllers import butterflyController
-from schemas.buterflySchema import Butterfly 
+from schemas.buterflySchema import Butterfly
+from schemas.buterflySchema import ButterflyCreate
+from database.database_connection import get_db
+from sqlalchemy.orm import Session
+from fastapi import Depends
 
 
 router = APIRouter()
 
 # Ruta "/butterflies/" GET para obtener todas las mariposas
-router.get("/butterflies/", response_model=list[Butterfly])(butterflyController.get_butterflies)
+@router.get("/butterflies/")
+def read_butterflies(db: Session = Depends(get_db)):
+        return butterflyController.get_butterflies(db)
+
+# Ruta "/butterflies/{butterfly_id}" GET para obtener una mariposa por su ID
+# Se define la ruta en la primera línea y la respuestas que dará la ruta.
+# En la segunda línea se define una función que recibe el parámetro butterfly_id y la Session de la base de datos, inyectada por el decorador Depends
+# en la tercera línea se llama al controlador que queremos asociar a esta ruta
+@router.get("/butterflies/{butterfly_id}", response_model=Butterfly)
+def read_one_butterfly(butterfly_id: int, db: Session = Depends(get_db)):
+    return butterflyController.get_butterfly_by_id(db, butterfly_id)
+
+
+# # Ruta "/butterflies/" POST para crear una mariposa
+@router.post("/butterflies", response_model=Butterfly)
+def create_butterfly(butterfly: ButterflyCreate, db: Session = Depends(get_db)):
+    return butterflyController.create_butterfly(db, butterfly)
+
+# # Ruta "/butterflies/{butterfly_id}" PUT para actualizar una mariposa por su ID
+# @router.put("/butterflies/{butterfly_id}", response_model=Butterfly)
+# def update_butterfly(butterfly_id: int, butterfly: Butterfly, db: Session = Depends(get_db)):
+#     return butterflyController.update_butterfly(db, butterfly_id, butterfly)
 
